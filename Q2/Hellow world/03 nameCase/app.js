7eb0638fa8082c43298063c6b9b0f7d1ce23e6c0
abida3232 <abidaasgher3232@gmail.com>
2024-05-20 19:02:04 +0500

    45 questions solutions

--- Q2/Hellow world/03 nameCase/app.js
+++
@@ -1,4 +0,0 @@
-var personName = "Abida Asgher";
-console.log(personName.toLowerCase());
-console.log(personName.toUpperCase());
-console.log(personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
--- Q2/Hellow world/03 nameCase/app.ts
+++
@@ -1,5 +0,0 @@
-let personName="Abida Asgher";
-
-console.log(personName.toLowerCase());
-console.log(personName.toUpperCase());
-console.log(personName.replace(/\b\w/g,(char)=>char.toUpperCase()));
